8c00a3bc196a09020fc550e05dc53f50b518bfc7
mikhail.kuziuk <mikhail.kuzyuk@gmail.com>
2024-01-03 17:11:27 +0300

    reworked news_api_url_service

---
+++ world_news_services/url/url.ts
@@ -0,0 +1,17 @@
+export type Url = {
+    text?: string;
+    sourceCountries?: string;
+    language?: string;
+    minSentiment?: number;
+    maxSentiment?: number;
+    earliestPublishDate?: string;
+    latestPublishDate?: string;
+    newsSources?: string;
+    authors?: string;
+    entities?: string;
+    locationFilter?: string;
+    sort?: string;
+    sortDirection?: string;
+    offset?: number;
+    number?: number;
+}
